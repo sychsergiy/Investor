0dc895d1b14d2be88d6bd8c166f229dfd1e5dae4
sychsergiy <sychsergiy@gmail.com>
2020-04-23 00:11:46 +0300

    [EAPI-1041] create payment draft added

--- investor/main.go
+++ investor/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"investor/asset/crypto_currency"
+	"investor/interactors"
+	"investor/payment"
 	"log"
 	"os"
 )
 
-func setupDependencies(coinMarketCupApiKey string) crypto_currency.RateFetcher {
+func setupDependencies(coinMarketCupApiKey string) interactors.PaymentCreator {
+
 	coinMarketCupClient := crypto_currency.NewCoinMarketCupClient(
 		"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest", coinMarketCupApiKey,
 	)
 	fetcher := crypto_currency.RateFetcher{
 		Client: coinMarketCupClient,
 	}
-	return fetcher
+
+	storage := payment.NewInMemoryStorage()
+	return interactors.PaymentCreator{Storage: storage, RateFetcher: fetcher}
 }
 func main() {
 	apiKey := os.Getenv("COIN_MARKET_CAP_API_KEY")
 	if apiKey == "" {
 		log.Fatal("COIN_MARKET_CAP_API_KEY env var not provided")
 	}
-	fetcher := setupDependencies(apiKey)
-	rate, err := fetcher.Fetch(crypto_currency.NewAsset(crypto_currency.BTC))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("BTC rate: %f", rate)
+	paymentCreator := setupDependencies(apiKey)
+	paymentCreator.Create()
 }
--- investor/payment/in_memory_storage.go
+++ investor/payment/in_memory_storage.go
@@ -1,10 +1,10 @@
 package payment
 
 type InMemoryStorage struct {
-	payments map[int]Record
+	payments map[int]Payment
 }
 
-func getNextID(payments map[int]Record) int {
+func getNextID(payments map[int]Payment) int {
 	max := -1
 	for key := range payments {
 		if key > max {
@@ -14,12 +14,12 @@ func getNextID(payments map[int]Record) int {
 	return max + 1
 }
 
-func (storage *InMemoryStorage) Create(payment Record) Identifier {
+func (storage *InMemoryStorage) Create(payment Payment) Identifier {
 	id := getNextID(storage.payments)
 	storage.payments[id] = payment
 	return Identifier(id)
 }
 
 func NewInMemoryStorage() *InMemoryStorage {
-	return &InMemoryStorage{payments: make(map[int]Record)}
+	return &InMemoryStorage{payments: make(map[int]Payment)}
 }
