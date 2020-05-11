b0a8f214e9048f06ae3ae86748e9297e43374066
sychsergiy <sychsergiy@gmail.com>
2020-05-11 03:14:09 +0300

    filter payments by period

--- investor/adapters/repositories/in_memory/payment_filters.go
+++ investor/adapters/repositories/in_memory/payment_filters.go
@@ -1,6 +1,9 @@
 package in_memory
 
-import "investor/entities/payment"
+import (
+	"investor/entities/payment"
+	"time"
+)
 
 func paymentTypesContains(paymentTypes []payment.Type, paymentType payment.Type) bool {
 	for _, pt := range paymentTypes {
@@ -11,7 +14,7 @@ func paymentTypesContains(paymentTypes []payment.Type, paymentType payment.Type)
 	return false
 }
 
-func FilterPaymentsByType(payments []payment.Payment, paymentTypes []payment.Type) (filtered []payment.Payment) {
+func FilterByType(payments []payment.Payment, paymentTypes []payment.Type) (filtered []payment.Payment) {
 	if len(paymentTypes) == 0 {
 		return payments
 	}
@@ -21,4 +24,26 @@ func FilterPaymentsByType(payments []payment.Payment, paymentTypes []payment.Typ
 		}
 	}
 	return filtered
+}
+
+func FilterByPeriod(payments []payment.Payment, periods []payment.Period) (filtered []payment.Payment) {
+	if len(periods) == 0 {
+		return payments
+	}
+	for _, payment_ := range payments {
+		for _, period := range periods {
+			if periodContains(period, payment_.CreationDate()) {
+				filtered = append(filtered, payment_)
+				break
+			}
+		}
+	}
+	return filtered
+}
+
+func periodContains(p payment.Period, date time.Time) bool {
+	if date.After(p.From()) && date.Before(p.Until()) {
+		return true
+	}
+	return false
 }
--- investor/adapters/repositories/in_memory/payment_filters_test.go
+++ investor/adapters/repositories/in_memory/payment_filters_test.go
@@ -13,7 +13,15 @@ func createPaymentWithType(paymentType payment.Type) payment.Payment {
 	)
 }
 
-func TestFilterPaymentsByType(t *testing.T) {
+func createPaymentWithCreationDate(year int) payment.Payment {
+	assetRecord := CreateAssetRecord("1", "test")
+	return payment.NewPlainPayment(
+		"1", 0, 0, assetRecord.ToAsset(),
+		payment.CreateYearDate(year), payment.Invest,
+	)
+}
+
+func TestFilterByType(t *testing.T) {
 	type unit struct {
 		payments          []payment.Payment
 		paymentTypes      []payment.Type
@@ -50,9 +58,44 @@ func TestFilterPaymentsByType(t *testing.T) {
 	}
 
 	for _, unit := range units {
-		res := FilterPaymentsByType(unit.payments, unit.paymentTypes)
+		res := FilterByType(unit.payments, unit.paymentTypes)
 		if len(res) != unit.expectedResultLen {
 			t.Errorf("Expected result len %d but got %d", unit.expectedResultLen, len(res))
+		}
+	}
+}
+
+func TestFilterByPeriod(t *testing.T) {
+	type unit struct {
+		payments          []payment.Payment
+		periods           []payment.Period
+		expectedResultLen int
+	}
+	p1 := payment.PeriodMock{
+		TimeFrom:  payment.CreateYearDate(2010),
+		TimeUntil: payment.CreateYearDate(2012),
+	}
+	p2 := payment.PeriodMock{
+		TimeFrom:  payment.CreateYearDate(2014),
+		TimeUntil: payment.CreateYearDate(2016),
+	}
+	payments := []payment.Payment{
+		createPaymentWithCreationDate(2009),
+		createPaymentWithCreationDate(2011),
+		createPaymentWithCreationDate(2013),
+		createPaymentWithCreationDate(2015),
+		createPaymentWithCreationDate(2017),
+	}
+	units := []unit{
+		{payments, []payment.Period{p1, p2}, 2},
+		{payments, []payment.Period{p1}, 1},
+		{payments, []payment.Period{p2}, 1},
+		{payments, []payment.Period{}, 5},
+	}
+	for _, u := range units {
+		res := FilterByPeriod(u.payments, u.periods)
+		if len(res) != u.expectedResultLen {
+			t.Errorf("Expected res len %d but got %d", u.expectedResultLen, len(res))
 		}
 	}
 }
